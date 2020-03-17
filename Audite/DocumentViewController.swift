22af2acd63bd1ea3d23119f24783ccac221c681b
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-03-16 22:36:02 -0300

    Initial Commit

---
+++ Audite/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  Audite
+//
+//  Created by Pedro Giuliano Farina on 16/03/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
